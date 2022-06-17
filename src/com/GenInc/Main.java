26f5de51e6c5c24843b4c15be4a09d6a6a352bea
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-06-17 21:30:57 +0300

    add input line normalization

--- src/com/GenInc/Main.java
+++ src/com/GenInc/Main.java
@@ -5,24 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите выражение");
         String line = scanner.nextLine();
-
-        String[] arr = line.split(" ");
-        switch (arr[1]) {
-            case ("+"):
-                System.out.println(Double.parseDouble(arr[0]) + Double.parseDouble(arr[2]));
-                break;
-            case ("-"):
-                System.out.println(Double.parseDouble(arr[0]) - Double.parseDouble(arr[2]));
-                break;
-            case ("*"):
-                System.out.println(Double.parseDouble(arr[0]) * Double.parseDouble(arr[2]));
-                break;
-            case ("/"):
-                System.out.println(Double.parseDouble(arr[0]) / Double.parseDouble(arr[2]));
-                break;
+        line = line.replace(" ", "");
+        String fnum;
+        String snum;
+        Double result;
+        char[] arrLine = line.toCharArray();
+        for (int i = 1; i < arrLine.length; i++) {
+            switch (line.charAt(i)){
+                case('+'):
+                    fnum = line.substring(0, i);
+                    snum = line.substring(i+1);
+                    result = Double.parseDouble(fnum) + Double.parseDouble(snum);
+                    System.out.println(result);
+                case('-'):
+                    fnum = line.substring(0, i);
+                    snum = line.substring(i+1);
+                    result = Double.parseDouble(fnum) - Double.parseDouble(snum);
+                    System.out.println(result);
+                case('*'):
+                    fnum = line.substring(0, i);
+                    snum = line.substring(i+1);
+                    result = Double.parseDouble(fnum) * Double.parseDouble(snum);
+                    System.out.println(result);
+                case('/'):
+                    fnum = line.substring(0, i);
+                    snum = line.substring(i+1);
+                    result = Double.parseDouble(fnum) / Double.parseDouble(snum);
+                    System.out.println(result);
+            }
         }
 
 
