810e401f08a7b51dfbeeba0cf6400537fb320eea
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-06-18 23:25:08 +0300

    add roman numbers possibility

--- src/com/GenInc/Main.java
+++ src/com/GenInc/Main.java
@@ -14,31 +14,47 @@ public class Main {
         String snum;
         Double result;
         char[] arrLine = line.toCharArray();
-        for (int i = 1; i < arrLine.length; i++) {
-            switch (line.charAt(i)){
-                case('+'):
-                    fnum = line.substring(0, i);
-                    snum = line.substring(i+1);
-                    result = Double.parseDouble(fnum) + Double.parseDouble(snum);
-                    System.out.println(result);
-                case('-'):
-                    fnum = line.substring(0, i);
-                    snum = line.substring(i+1);
-                    result = Double.parseDouble(fnum) - Double.parseDouble(snum);
-                    System.out.println(result);
-                case('*'):
-                    fnum = line.substring(0, i);
-                    snum = line.substring(i+1);
-                    result = Double.parseDouble(fnum) * Double.parseDouble(snum);
-                    System.out.println(result);
-                case('/'):
-                    fnum = line.substring(0, i);
-                    snum = line.substring(i+1);
-                    result = Double.parseDouble(fnum) / Double.parseDouble(snum);
-                    System.out.println(result);
+        if ((arrLine[0] == '-') && ((arrLine[1] == 'I') ||
+                                    (arrLine[1] == 'V') ||
+                                    (arrLine[1] == 'X') ||
+                                    (arrLine[1] == 'l') ||
+                                    (arrLine[1] == 'C') ||
+                                    (arrLine[1] == 'D') ||
+                                    (arrLine[1] == 'M'))) {
+            System.out.println("invalid input");
+        } else if ((arrLine[0] == 'I') ||
+                    (arrLine[0] == 'V') ||
+                    (arrLine[0] == 'X') ||
+                    (arrLine[0] == 'l') ||
+                    (arrLine[0] == 'C') ||
+                    (arrLine[0] == 'D') ||
+                    (arrLine[0] == 'M')) {
+            System.out.println("it's roman");
+        } else {
+            for (int i = 1; i < arrLine.length; i++) {
+                switch (line.charAt(i)) {
+                    case ('+'):
+                        fnum = line.substring(0, i);
+                        snum = line.substring(i + 1);
+                        result = Double.parseDouble(fnum) + Double.parseDouble(snum);
+                        System.out.println(result);
+                    case ('-'):
+                        fnum = line.substring(0, i);
+                        snum = line.substring(i + 1);
+                        result = Double.parseDouble(fnum) - Double.parseDouble(snum);
+                        System.out.println(result);
+                    case ('*'):
+                        fnum = line.substring(0, i);
+                        snum = line.substring(i + 1);
+                        result = Double.parseDouble(fnum) * Double.parseDouble(snum);
+                        System.out.println(result);
+                    case ('/'):
+                        fnum = line.substring(0, i);
+                        snum = line.substring(i + 1);
+                        result = Double.parseDouble(fnum) / Double.parseDouble(snum);
+                        System.out.println(result);
+                }
             }
         }
-
-
     }
 }
